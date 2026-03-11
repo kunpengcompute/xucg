c5839f5034bb48fd652318c9cbf4449255b60946
onionddd <d30059792@china.huawei.com>
2026-03-11 16:06:09 +0800

    set gather/gatherv plan policy

--- src/planc/ucx/gatherv/gatherv.c
+++ src/planc/ucx/gatherv/gatherv.c
@@ -26,17 +26,17 @@ static ucg_plan_attr_t ucg_planc_ucx_gatherv_plan_attr[] = {
 
 static ucg_config_field_t gatherv_config_table[] = {
 
-    {"GATHERV_KNTREE_DEGREE", "2",
+    {"GATHERV_KNTREE_DEGREE", "4",
      "Configure the k value in kntree algo for gatherv",
      ucg_offsetof(ucg_planc_ucx_gatherv_config_t, kntree_degree),
      UCG_CONFIG_TYPE_INT},
 
-    {"GATHERV_NA_KNTREE_INTER_DEGREE", "2",
+    {"GATHERV_NA_KNTREE_INTER_DEGREE", "4",
      "Configure the k value between nodes in node-aware kntree algo for gatherv",
      ucg_offsetof(ucg_planc_ucx_gatherv_config_t, na_kntree_inter_degree),
      UCG_CONFIG_TYPE_INT},
 
-    {"GATHERV_NA_KNTREE_INTRA_DEGREE", "2",
+    {"GATHERV_NA_KNTREE_INTRA_DEGREE", "4",
      "Configure the k value in a node in node-aware kntree algo for gatherv",
      ucg_offsetof(ucg_planc_ucx_gatherv_config_t, na_kntree_intra_degree),
      UCG_CONFIG_TYPE_INT},
@@ -50,7 +50,8 @@ UCG_PLAN_ATTR_REGISTER_TABLE(ucg_planc_ucx, UCG_COLL_TYPE_GATHERV,
                              ucg_planc_ucx_gatherv_plan_attr);
 
 static ucg_plan_policy_t gatherv[] = {
-    {3,  {0, UCG_PLAN_RANGE_MAX}, UCG_PLAN_UCX_PLAN_SCORE_1ST},
+    {4,  {0, UCG_PLAN_RANGE_MAX}, UCG_PLAN_UCX_PLAN_SCORE_1ST},
+    {3,  {0, UCG_PLAN_RANGE_MAX}, UCG_PLAN_UCX_PLAN_SCORE_2ND},
     UCG_PLAN_LAST_POLICY,
 };
 
